78c1dc581f7782d3c5284236385a1de72947fb2a
lucasSeabraGomes <39420989+lucasSeabraGomes@users.noreply.github.com>
2024-04-22 21:45:24 -0300

    Add files via upload

---
+++ trabalhoTrinity/src/ContagemPalavra.java
@@ -0,0 +1,8 @@
+public class ContagemPalavra {
+    public Integer quantidade;
+    public String Palavra;
+    @Override
+    public String toString(){
+        return Palavra+" = "+quantidade;
+    }
+}
