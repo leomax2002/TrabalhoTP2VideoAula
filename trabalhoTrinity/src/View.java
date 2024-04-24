fa081a4e2b42560ea2474a75731fc856e2038f63
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2024-04-23 22:26:46 -0300

    Add files via upload

---
+++ trabalhoTrinity/src/View.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+public class View {
+    private final Model model=new Model();
+    private ArrayList<ContagemPalavra> arrayPalavras=new ArrayList<>();
+    private int tamanho;
+    private HashMap<String,Integer> wordHash= new HashMap<>();
+    private String[] dadosArray;
+    public void caregarWordCount(String nomeArquivo)  {
+        this.dadosArray = model.formataTexto(nomeArquivo);
+        this.tamanho = this.dadosArray.length;
+        if(tamanho >= 5000){
+            HashMap<String,Integer> map= model.contarPalavras(dadosArray, wordHash);
+            this.arrayPalavras=converteEmLista(map);
+            String[] newArray = new String[dadosArray.length - 5000];
+            System.arraycopy(dadosArray, 5000, newArray, 0, dadosArray.length - 5000);
+            this.dadosArray = newArray;
+        }
+        else{
+            HashMap<String,Integer> map= model.contarPalavras(dadosArray, wordHash);
+            this.arrayPalavras=converteEmLista(map);
+        }
+    }
+
+    public void continuaWord(){
+        this.tamanho = this.dadosArray.length;
+        if(tamanho >= 5000){
+            HashMap<String,Integer> map= model.contarPalavras(dadosArray, wordHash);
+            this.arrayPalavras=converteEmLista(map);
+            String[] newArray = new String[dadosArray.length - 5000];
+            System.arraycopy(dadosArray, 5000, newArray, 0, dadosArray.length - 5000);
+            this.dadosArray = newArray;
+        }
+        else{
+            HashMap<String,Integer> map= model.contarPalavras(dadosArray, wordHash);
+            this.arrayPalavras=converteEmLista(map);
+        }
+    }
+
+    public List<ContagemPalavra> apresentarArrayWordCount(int qtdPagina, int numPagina, int tipoOrdenacao){
+        ordenarArray(tipoOrdenacao);
+        if(qtdPagina*(numPagina+1)<arrayPalavras.size()) {
+            return this.arrayPalavras.subList(qtdPagina * numPagina, qtdPagina * (numPagina + 1));
+        }else{
+            return this.arrayPalavras.subList(qtdPagina * numPagina-1, arrayPalavras.size()-1);
+        }
+    }
+    private ArrayList<ContagemPalavra> converteEmLista(HashMap<String,Integer> map){
+        ArrayList<ContagemPalavra> arrayPalavras= new ArrayList<>();
+        for (String key : map.keySet()) {
+            ContagemPalavra objeto = new ContagemPalavra();
+            objeto.Palavra= key;
+            objeto.quantidade= map.get(key);
+            arrayPalavras.add(objeto);
+        }
+        return arrayPalavras;
+    }
+    private void ordenarArray(int cresceteOuDecrescente){
+        System.out.println(arrayPalavras.size());
+        if(cresceteOuDecrescente<0){
+            Comparator<ContagemPalavra> comparator = Comparator
+                    .comparing(e -> (-1)*e.quantidade);
+            arrayPalavras.sort(comparator);
+        }
+        else{
+            Comparator<ContagemPalavra> comparator = Comparator
+                    .comparing(e -> e.quantidade);
+            arrayPalavras.sort(comparator);
+        }
+    }
+
+    public int getTamanho(){
+        return this.tamanho;
+    }
+}
